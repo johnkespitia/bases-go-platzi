7234ba1ef88c6b8e7b097c2a138e2930e650b368
johnk_ <john.espitia@dafiti.com.co>
2020-02-14 18:34:03 -0500

    Inclusión de manejo de execiones errores

--- github.com/johnksft/gocurso/main.go
+++ github.com/johnksft/gocurso/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/johnksft/gocurso/structs"
 )
 
@@ -8,4 +11,22 @@ const helloWorld string = " Hola %s %s bienvenido \n"
 
 func main() {
 	structs.InterfaceTest()
+	res, err := sumaRegular(3, 3)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res)
+}
+
+func sumaRegular(num1 interface{}, num2 interface{}) (int, error) {
+	switch num1.(type) {
+	case string:
+		return 0, errors.New("El primer valor es un String")
+	}
+	switch num2.(type) {
+	case string:
+		return 0, errors.New("El segundo valor es un String")
+	}
+
+	return num1.(int) + num2.(int), nil
 }
