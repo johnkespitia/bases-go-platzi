b283cb3fd6ce7bd05833bac520f5a8f4e4af744c
johnk_ <john.espitia@dafiti.com.co>
2020-02-14 17:48:02 -0500

    Inclusión de interfaces y defer

--- github.com/johnksft/gocurso/main.go
+++ github.com/johnksft/gocurso/main.go
@@ -1,39 +1,11 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/johnksft/gocurso/maps"
+	"github.com/johnksft/gocurso/structs"
 )
-
-type PlatziCourse struct {
-	Name   string
-	Slug   string
-	Skills []string
-}
-
-func (p PlatziCourse) Subscribe(name string) {
-	fmt.Printf("La persona %s se ha registrado al curso %s \n", name, p.Name)
-}
-
-//Herencia de Structs
-type PlatziCarrer struct {
-	PlatziCourse
-}
 
 const helloWorld string = " Hola %s %s bienvenido \n"
 
 func main() {
-	mapa := maps.GetMap("Juan")
-	fmt.Println(mapa)
-	platziCourse := PlatziCourse{
-		Name: "Go Lang", Slug: "/go-lang", Skills: []string{"Backend", "2"},
-	}
-	platziCourse1 := new(PlatziCourse)
-	platziCourse1.Name = "Go1"
-	platziCourse1.Slug = "/go"
-	platziCourse1.Skills = []string{"backend"}
-	fmt.Println(platziCourse)
-	platziCourse.Subscribe("Johnk")
-	fmt.Println(platziCourse1)
+	structs.InterfaceTest()
 }
